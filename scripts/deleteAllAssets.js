000c5b89f2f067296bb2b4b9a124b7aab1d91940
Julia⭐️ <julesrsilver@gmail.com>
2022-04-12 15:11:52 -0700

    scripts

---
+++ scripts/deleteAllAssets.js
@@ -0,0 +1,11 @@
+import client from 'part:@sanity/base/client'
+const sanityClient = client.withConfig({apiVersion: '2022-04-10'})
+
+sanityClient
+  .delete({
+    query: '*[_type == "sanity.imageAsset"][0...999]'
+  })
+  .then(console.log)
+  .catch(console.error)
+
+// sanity exec scripts/deleteAllAssets.js --with-user-token
---
+++ scripts/deleteDrafts.js
@@ -0,0 +1,11 @@
+import client from 'part:@sanity/base/client'
+const sanityClient = client.withConfig({apiVersion: '2022-04-10'})
+
+sanityClient
+  .delete({
+    query: '*[_id match "drafts*"][0...999]'
+  })
+  .then(console.log)
+  .catch(console.error)
+
+// sanity exec scripts/deleteDrafts.js --with-user-token
